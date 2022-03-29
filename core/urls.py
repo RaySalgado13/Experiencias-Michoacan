3de7bf5f79b2a9f307caf17a346b91d0e4160bca
Ray Salgado <19120236@morelia.tecnm.mx>
2022-03-29 17:45:16 -0600

    Aplicaciones turista empresa y asociacion configuradas

--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,8 @@ from django.urls import path, include  # add this
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
-    path("", include("apps.home.urls"))             # UI Kits Html files
+    #path("", include("apps.home.urls")), IGNORAR
+    path("", include("apps.turista.urls")),           # UI Kits Html files
+    path("empresas/", include("apps.empresas.urls")),             # UI Kits Html files
+    path("asociacion/", include("apps.asociacion.urls"))             # UI Kits Html files
 ]
