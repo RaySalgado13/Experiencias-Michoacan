9ce9a838c3eb61590663008a7163c7e7799959b0
Ray Salgado <19120236@morelia.tecnm.mx>
2022-04-29 14:55:39 -0500

    Cambios en archivos de configuracion de BD

--- apps/home/models.py
+++ apps/home/models.py
@@ -11,7 +11,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Imagen(models.Model):
     image = models.FileField(null=False, blank = False)
-    date_uploaded = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.image
 
 
 class Producto(models.Model):
@@ -24,6 +28,8 @@ class Producto(models.Model):
     empresa = models.ForeignKey('Empresa', on_delete=models.CASCADE)
     tipo = models.ForeignKey('Tipo_producto' , on_delete=models.CASCADE)
     imagen = models.ManyToManyField(Imagen)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.nombre}  {self.descripcion[0:50]}"
@@ -36,6 +42,8 @@ class Empresa(models.Model):
     telefono = models.CharField(max_length=10)
     representante = models.CharField(max_length=200)
     direccion = models.ForeignKey('Direccion', on_delete=models.CASCADE) 
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nombre_legal
@@ -49,6 +57,8 @@ class Direccion(models.Model):
     cp = models.CharField(max_length=10)
     ciudad = models.CharField(max_length=100)
     estado = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.calle[0:50]} {self.numero_exterior} {self.cp}"
@@ -56,6 +66,8 @@ class Direccion(models.Model):
 
 class Tipo_producto(models.Model):
     tipo = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.tipo
@@ -69,6 +81,8 @@ class Reservacion(models.Model):
     fecha = models.DateTimeField(auto_now = True)
     status = models.CharField(max_length=50)
     producto = models.ManyToManyField(Producto)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.folio
@@ -80,12 +94,16 @@ class Reporte(models.Model):
     monto_total = models.IntegerField()
     items = models.JSONField()
     tipo = models.ForeignKey('Tipo_reporte', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.titulo}"
 
 class Tipo_Reporte(models.Model):
     tipo = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.tipo
