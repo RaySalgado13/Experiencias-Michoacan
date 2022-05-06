75c630db4f882805695c1bb0be433e329878b7cb
Ray Salgado <19120236@morelia.tecnm.mx>
2022-05-06 13:23:56 -0500

    Modificacion de template 404

--- core/settings.py
+++ core/settings.py
@@ -66,8 +66,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 ROOT_URLCONF = 'core.urls'
-LOGIN_REDIRECT_URL = "home"  # Route defined in home/urls.py
-LOGOUT_REDIRECT_URL = "home"  # Route defined in home/urls.py
+LOGIN_REDIRECT_URL = ""  # Route defined in home/urls.py
+LOGOUT_REDIRECT_URL = "login"  # Route defined in home/urls.py
 TEMPLATE_DIR = os.path.join(CORE_DIR, "apps/templates")  # ROOT dir for templates
 
 TEMPLATES = [
--- core/urls.py
+++ core/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
     path("home/", include("apps.home.urls"), name='home'),
-    path("", include("apps.turista.urls")),
+    path("", include("apps.turista.urls"), name="inicio"),
     path("empresas/", include("apps.empresas.urls")),
     path("asociacion/", include("apps.asociacion.urls")),
 ]
